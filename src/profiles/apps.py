94c43d32cee182b73947c7747d712ebe2f2ee982
kkamara <kelvinkamara@protonmail.com>
2022-03-26 09:48:06 +0000

    Add profile model signals

--- src/profiles/apps.py
+++ src/profiles/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class ProfilesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'src.profiles'
+  default_auto_field = 'django.db.models.BigAutoField'
+  name = 'src.profiles'
+
+  def ready(self):
+    from src.profiles import signals
+    
+
